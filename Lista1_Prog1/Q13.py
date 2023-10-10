9998e7daa5b903cadef1037398b09ab60260a946
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2023-10-10 18:53:04 +0000

    update

--- Lista1_Prog1/Q13.py
+++ Lista1_Prog1/Q13.py
@@ -18,8 +18,15 @@ def buscar_numero_inteiro_na_lista(lista_numeros_inteiros, numero_buscado):
         contador += 1
     return False, contador
 
-lista = [1, 45, 34, 67, 2, 15, 77, 102]
-print(buscar_numero_inteiro_na_lista(lista, 10))
-print(buscar_numero_inteiro_na_lista(lista, 1))
-print(buscar_numero_inteiro_na_lista(lista, 35))
-print(buscar_numero_inteiro_na_lista(lista, 2))
+from random import randint
+
+def gerar_aleatorio(quantidade, limite):
+    return [randint(0, limite) for _ in range(quantidade)]
+
+soma = 0
+for _ in range(1000):
+    lista = gerar_aleatorio(1000, 500)
+    lista.sort()
+    _, cont = buscar_numero_inteiro_na_lista(lista, randint(1, 500))
+    soma += cont
+print(soma/1000)
--- Lista1_Prog1/Q16.py
+++ Lista1_Prog1/Q16.py
@@ -15,9 +15,9 @@ def verificar_frerquencia_valor(lista_elementos, elemento_desejado):
             frequencia += 1
     return frequencia
 
-lista = []
-for i in range(1000):
-    lista.append(randint(1, 101))
+def gerar_aleatorio(quantidade, limite):
+    return [randint(0, limite) for _ in range(quantidade)]
 
+lista = gerar_aleatorio(1000, 101)
 print(f'{lista}\n')
 print(verificar_frerquencia_valor(lista, 101))
