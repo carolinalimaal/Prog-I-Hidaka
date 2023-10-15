61f1581a9a84d824de129c56537c7fca2754c2d0
carolinalimaal <carol.lima.alves2005@gmail.com>
2023-10-14 23:08:23 -0300

    update

--- Lista1_Prog1/functions.py
+++ Lista1_Prog1/functions.py
@@ -1,135 +1,159 @@
 # Questão 01
-def concatenar_strings_v1(lista_1: list, lista_2: list):
-    """
-    Recebe duas listas de strings de mesmo tamanho e cria outra lista de strings com
-    o resultado da concatenação dos elementos correspondentes das listas iniciais.
-
-    Argumentos: 
-        lista_strings_1, lista_strings_2.
-    Saída: 
-        lista_concatenada.
-    """
-    resultado = []
-    for x, y in zip(lista_1, lista_2):
-        resultado.append(x + y)
-    return resultado
-
-
-def concatenar_strings_v2(lista_1: list, lista_2: list):
-    """
-    Recebe duas listas de strings de mesmo tamanho e cria outra lista de strings com
-    o resultado da concatenação dos elementos correspondentes das listas iniciais.
-    MÉTODO LIST COMPREHENSION.
-
-    Argumentos: 
-        lista_strings_1, lista_strings_2.
-    Saída: 
-        lista_concatenada.
-    """
-    return [x + y for x, y in zip(lista_1, lista_2)]
+def concatenar_elementos(lista1: list, lista2: list):
+    lista_concatenada = []
+    for x, y in zip(lista1, lista2):
+        lista_concatenada.append(x + y)
+    return lista_concatenada
 
 # Questão 02
-def verificar_numero_primo(numero: int):
-    """
-    Verifica se o número de entrada é primo ou não.
-    
-    Argumento: 
-        numero_inteiro
-    Saída: 
-        Valor booleano True (para é primo) ou False (para não é primo)
-    """
+def encontrar_primo(numero: int):
     for divisor in range(2, numero):
         if numero % divisor == 0:
             return False
     return True
 
-
-def encontrar_proximo_primo(numero: int):
-    """
-    Encontra o próximo primo em relação ao número de entrada. 
-    Não leva em consideração o número de entrada, mesmo que ele seja primo.
-    
-    Argumento: 
-        numero_inteiro
-    Saída: 
-        numero_inteiro_primo
-    """
+def encontrar_proximo_primo(numero:int):
     numero += 1
-    while not verificar_numero_primo(numero):
+    while not encontrar_primo(numero):
         numero += 1
     return numero
 
-
-def mapear_lista_numeros_primos(lista_inteiros: list):
-    """
-    Mapea uma lista de números para uma lista dos menores números 
-    primos maiores que os números da lista inicial.
-    
-    Argumento: 
-        lista de inteiros
-    Saída: 
-        Lista de números inteiros primos.
-    """
+def mapear_primos(lista_inteiros: list):
     return [encontrar_proximo_primo(item) for item in lista_inteiros]
 
 # Questão 03
-def filtrar_strings_por_tamanho_v1(lista_strings: list):
-    """
-    Filtra uma lista de strings para uma outra lista contendo 
-    somente strings com tamanho menor que 10.
-    
-    Argumento: 
-        lista_strings
-    Saída: 
-        lista_strings_filtrada
-    """
+def filtrar_strings_por_tamanho(lista_strings: list):
     lista_filtrada = []
     for item in lista_strings:
         if len(item) < 10:
             lista_filtrada.append(item)
-    return sorted(lista_filtrada)
-
-
-def filtrar_strings_por_tamanho_v2(lista_strings):
-    """
-    Filtra uma lista de strings para uma outra lista contendo
-    somente strings com tamanho menor que 10.
-    MÉTODO LIST COMPREHESION
-    
-    Argumento: 
-        Lista de strings
-    Saída: 
-        Lista de strings filtrada
-    """
-    return sorted([item
-                   for item in lista_strings
-                   if len(item) < 10])
+    return lista_filtrada
 
 # Questão 04
-def 
+def filtrar_por_tipo(lista: list, tipo: type):
+    lista_filtrada = []
+    for item in lista:
+        if type(item) == tipo:
+            lista_filtrada.append(item)
+    return lista_filtrada
+
+def filtrar_listas_hetereogenas(lista: list):
+    lista_map = []
+    for sublista in lista:
+        lista_map.append(filtrar_por_tipo(sublista, int))
+    return lista_map
 
 # Questão 05
+def filtrar_strings(lista_strings: list, lista_inteiros: list):
+    lista_filtrada = []
+    for string, inteiro in zip(lista_strings, lista_inteiros):
+        if len(string) <= inteiro:
+            lista_filtrada.append(string)
+    return lista_filtrada
 
 # Questão 06
+def filtrar_sublistas(lista: list):
+    lista_filtrada = []
+    for idx in range(0, len(lista) - 1):
+        if sum(lista[idx]) > sum(lista[idx + 1]):
+            lista_filtrada.append(lista[idx])
+    return lista_filtrada
 
 # Questão 07
+def encontrar_menor_soma(lista1: list, lista2: list, lista3: list):
+    menor = lista1[0] + lista2[0] + lista3[0]
+    for x, y, z in zip(lista1, lista2, lista3):
+        menor = (((x + y + z) + menor) - abs((x + y + z) - menor) ) // 2
+    return menor
 
 # Questão 08
+def encontrar_maior_string(lista_strings: list):
+    tamanho = len(lista_strings[0])
+    idx = 0
+    for item in lista_strings:
+        if len(item) > tamanho:
+            tamanho = len(item)
+            idx = lista_strings.index(item)
+    return f'A maior string é {lista_strings[idx]}, com {tamanho} de tamanho e está no indice {idx}'
 
 # Questão 09
+def somar_valores_reais(lista: list):
+    soma = 0
+    for idx in range(0, len(lista) - 1):
+        if lista[idx] > lista[idx + 1]:
+            soma += lista[idx]
+    return soma
 
 # Questão 10
+def concatenar_strings_separadas_por_vírgula_espaco(lista: list):
+    string_concatenada = ''
+    for string in lista:
+        for elemento in string:
+            if elemento != ' ':
+                string_concatenada += elemento
+    return string_concatenada
 
 # Questão 11
+def encontrar_maior_primo(lista: list):
+    maior = -1
+    idx_maior = -1
+    for item in lista:
+        if encontrar_primo(item):
+            maior = (maior + item + abs(maior - item)) // 2
+            idx_maior = lista.index(maior)
+    return maior, idx_maior
 
 # Questão 12
+def calcular_desvio_padrão(lista: list):
+    soma = 0
+    for item in lista:
+        soma += item
+    media = soma / len(lista)
+    variancia = 0
+    for item in lista:
+        variancia += (item - media)**2
+    desvio_padrao = (variancia / len(lista))**(1/2)
+    return desvio_padrao
 
 # Questão 13
+import random as r
+def gerar_aleatorio(quantidade: int, limite: int):
+    return [r.randint(0, limite) for _ in range(quantidade)]
+
+def busca_linear(lista: list, valor: int):
+    comparacoes = 0
+    for item in lista:
+        comparacoes += 1
+        if item == valor:
+            return True, comparacoes
+    return False, comparacoes
 
 # Questão 14
+def busca_linear_ordenada(lista: list, valor: int):
+    lista = sorted(lista)
+    comparacoes = 0
+    for item in lista:
+        comparacoes += 1
+        if item == valor:
+            return True, comparacoes
+    return False, comparacoes
 
 # Questão 15
-
-# Questão 16
-
-# Questão 17
+def busca_binaria(lista: list, valor:int):
+    comparacoes = 0 
+    I = 0 
+    F = len(lista) - 1
+    M = (I + F) // 2
+    while I <= F:
+        if lista[M] == valor:
+            comparacoes += 1
+            return True, comparacoes
+        elif lista[M] > valor:
+            comparacoes += 1
+            F = M - 1
+            M = (I + F) // 2
+        else:
+            comparacoes += 1
+            I = M + 1
+            M = (I + F) // 2
+    return False, comparacoes
--- Lista1_Prog1/main.py
+++ Lista1_Prog1/main.py
@@ -1,16 +1,67 @@
 import functions as f
 
-# Questão 1
-lista_strings_1 = ['Até ', 'Eu amo ', 'Boa ']
-lista_strings_2 = ['logo.', 'Belém', 'jogada!']
-print(f.concatenar_strings_v1(lista_strings_1, lista_strings_2))
-print(f.concatenar_strings_v2(lista_strings_1, lista_strings_2))
+# Questão 01
+lista_strings1 = ['a', 'b', 'c', 'd']
+lista_strings2 = ['w', 'x', 'y', 'z']
+print(f.concatenar_elementos(lista_strings1, lista_strings2))
 
-# Questão 2
-lista_inteiros = [1, 4, 25, 8, 9, 11, 19, 59, 7]
-print(f.mapear_lista_numeros_primos(lista_inteiros))
+# Questão 02
+numeros_inteiros = [2, 6, 4, 9, 15, 27]
+print(f.mapear_primos(numeros_inteiros))
 
-# Questão 3
-lista_strings = ['Gabriel', 'Pedro', 'Carolina', 'Paralelepipedo']
-print(f.filtrar_strings_por_tamanho_v1(lista_strings))
-print(f.filtrar_strings_por_tamanho_v2(lista_strings))
+# Questão 03
+strings = ['Carolina', 'Paralelepipedo', 'Maria', 'Até logo!']
+print(f.filtrar_strings_por_tamanho(strings))
+
+# Questão 04
+lista_heterogenea = [[1, 4.5, 'bom dia', 8], [9.0, 'carol', 19, 2.78]]
+print(f.filtrar_listas_hetereogenas(lista_heterogenea))
+
+# Questão 05
+strings = ['Carolina', 'Gabriel', 'Sony', 'Nintendo']
+inteiros = [7, 9, 3, 10]
+print(f.filtrar_strings(strings, inteiros))
+
+# Questão 06
+lista_sublistas = [[5, 2, 7], [5, 8, 0], [11, 1, 3], []]
+print(f.filtrar_sublistas(lista_sublistas))
+
+#Questão 07
+l1 = [3, 1, 8]
+l2 = [4, 6, 5]
+l3 = [9, 4, 1]
+print(f.encontrar_menor_soma(l1, l2, l3))
+
+# Questão 08
+strings = ['Gabe', 'Infernape', 'Lucario', 'Garchomp']
+print(f.encontrar_maior_string(strings))
+
+# Questão 09
+num_reais = [2.0, 1.978, 0.89764, 5.072395, 5.000017]
+print(f.somar_valores_reais(num_reais))
+
+# Questão 10
+strings = ['Eu', 'amo ', 'o frio!', ' -As pessoas', 'são mais elegantes.']
+print(f.concatenar_strings_separadas_por_vírgula_espaco(strings))
+
+# Questão 11
+inteiros = [3, 5, 9, 11, 2, 13, 17]
+print(f.encontrar_maior_primo(inteiros))
+
+# Questão 12
+numeros = [1.55, 1.7, 1.8]
+print(f.calcular_desvio_padrão(numeros))
+
+# Questão 13
+lista_numeros = f.gerar_aleatorio(100, 50)
+print(lista_numeros)
+print(f.busca_linear(lista_numeros, 1))
+
+# Questão 14
+print(sorted(lista_numeros))
+print(f.busca_linear_ordenada(lista_numeros, 1))
+
+# Questão 15
+lista_numeros = f.gerar_aleatorio(10, 5)
+print(sorted(lista_numeros))
+print(f.busca_binaria(sorted(lista_numeros), 1))
